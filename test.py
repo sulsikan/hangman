32b114909ec9922e51d6e40f5da04d27341edfc5
주언 <137984851+sulsikan@users.noreply.github.com>
2025-06-03 18:40:24 +0900

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ class HangmanTestCase(unittest.TestCase):
     # checkCorrectAnswer(correctLetters, secretWord)
     def test_checkCorrectAnswer(self):
         answer = hangman.checkCorrectAnswer("baon", "baboon")
-        self.assertTrue(answer)
+        self.assertTrue(answer)  # assertTrue는 TestCase에서 계승된 함수이다. answer가 true가 아니면 에러 반환
 
     def test_checkWrongAnswer(self):
         answer = hangman.checkWrongAnswer("zebrio", "zebra")
@@ -19,7 +19,7 @@ class HangmanTestCase(unittest.TestCase):
 
     def test_1(self):
         answer = hangman.checkCorrectAnswer("bazn", "baboon")
-        self.assertFalse(answer)
+        self.assertFalse(answer) # assertTrue의 반대. answer가 false가 아니면 에러 반환
 
     def test_2(self):
         answer = hangman.checkCorrectAnswer("", " ")
@@ -28,7 +28,10 @@ class HangmanTestCase(unittest.TestCase):
     def test_3(self):
         answer = hangman.checkCorrectAnswer("ZEBRA", "zebra")
         self.assertFalse(answer)
-
-
-if __name__ == "__main__":
-    unittest.main()
+'''
+파이썬 파일은 실행될 때마다 **__name__**이라는 특별한 변수를 자동으로 생성해요.
+이 파일이 직접 실행되면 → __name__은 "__main__"이 돼요.
+하지만 다른 파일에서 import되면 → __name__은 파일 이름("test_hangman" 등)이 돼요.
+'''
+if __name__ == "__main__":   # 이 파일이 직접 실행된 경우에만 아래 코드를 실행해라
+    unittest.main()          # unittest.main()은 unittest 모듈의 내장 함수예요. 위에서 정의한 HangmanTestCase 클래스의 모든 test_로 시작하는 함수들을 자동 실행해요. 결과를 콘솔에 예쁘게 출력해줍니다 (통과/실패 여부 등).
